38a98387ba6f0d6587dcca1ee48886f0e16930de
vtv110 <74960681+vtv110@users.noreply.github.com>
2021-10-10 16:12:25 +0300

    Вторая часть дз к первому уроку

---
+++ lesson1 part2.py
@@ -0,0 +1,12 @@
+#Вторая часть дз 
+
+percent = int(input('Введите число процента: '))
+if percent == 1:
+    word = 'процент'
+elif percent <= 4:
+    word = 'процента'
+else:
+    word = 'процентов'
+print(percent, word)
+
+
